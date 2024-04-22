211eb8c038fd07f626c495991208dcfeadbd75a0
WhoInGodsName <61045310+WhoInGodsName@users.noreply.github.com>
2024-04-22 22:20:53 +0800

    Add project files.

---
+++ ContentWarningMod/CWMod.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace ContentWarningMod
+{
+    internal class CWMod : MonoBehaviour
+    {
+        Player localPlayer;
+        string x = "";
+        string y = "";
+        public void OnGUI()
+        {
+            Render.Begin("Risk of Tears 1.0.1", 4f, 1f, 180f, 750f, 10f, 20f, 2f);
+            Render.Label(x);
+            Render.Label(y);
+        }
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+            UpdateLocalPlayer();
+            localPlayer.data.health = 99999f;
+            x = (localPlayer.data.health).ToString();
+            y = (localPlayer.data.remainingOxygen).ToString();
+
+
+        }
+
+        public void UpdateLocalPlayer()
+        {
+            foreach (Player player in Player.FindObjectsOfType(typeof(Player)))
+            {
+                if (player != null && player.IsLocal)
+                {
+                    localPlayer = player;
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ ContentWarningMod/Loader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ContentWarningMod
+{
+    public class Loader : MonoBehaviour
+    {
+        public static void Init()
+        {
+            Loader.Load = new GameObject();
+            Loader.Load.AddComponent<CWMod>();
+            UnityEngine.Object.DontDestroyOnLoad(Loader.Load);
+        }
+
+        public static void Unload()
+        {
+            _Unload();
+        }
+
+        private static void _Unload()
+        {
+            GameObject.Destroy(Load);
+        }
+
+        private static GameObject Load;
+    }
+}
